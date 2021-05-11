740a076179671ec77709237da88fa49c5b3c2f5e
kawww <39939137+kawww@users.noreply.github.com>
2021-05-12 03:23:01 +0800

    Add files via upload

---
+++ curl.php
@@ -0,0 +1,42 @@
+<?php
+
+$postfields="get_blockchain_state";
+
+$url="https://localhost/".$postfields;
+
+
+
+            $postData = json_encode($postfields);
+            
+        
+       
+   $ch = curl_init();
+   $params[CURLOPT_URL] = $url;   
+   $params[CURLOPT_HEADER] = false; 
+   $params[CURLOPT_RETURNTRANSFER] = true; 
+   $params[CURLOPT_FOLLOWLOCATION] = true; 
+   $params[CURLOPT_POST] = true;
+   $params[CURLOPT_PORT] = 8555;
+   $params[CURLOPT_POSTFIELDS] = $postData;
+   $params[CURLOPT_SSL_VERIFYPEER] = false;
+   $params[CURLOPT_SSL_VERIFYHOST] = false;
+
+	$params[CURLOPT_SSLCERTTYPE] = 'PEM';
+	$params[CURLOPT_SSLCERT] = 'pcrt.pem';
+	$params[CURLOPT_SSLKEYTYPE] = 'PEM';
+	$params[CURLOPT_SSLKEY] = 'pkey.pem';
+
+     curl_setopt_array($ch, $params); 
+     $content = curl_exec($ch); 
+	$output = curl_getinfo($ch);
+     curl_close($ch);
+
+
+
+echo $content."<br>";
+
+//print_r($output);
+
+
+
+?>
